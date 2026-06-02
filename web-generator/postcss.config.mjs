e4b94490ea3781f5f06e4ce9aab01f46a43994da
Wynames <108218174+Wynames@users.noreply.github.com>
2026-06-02 14:13:25 +0700

    Update postcss.config.mjs

--- web-generator/postcss.config.mjs
+++ web-generator/postcss.config.mjs
@@ -1,10 +1,4 @@
-// file: web-generator/postcss.config.mjs
+// web-generator/postcss.config.mjs
 /** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
-
+const config = { plugins: { tailwindcss: {}, autoprefixer: {} } };
 export default config;
